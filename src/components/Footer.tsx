cd9f4df4b30b1012dc06bf4dff23f6d432f4b649
Lakshan Suresh <slakshan30@gmail.com>
2026-01-04 16:32:31 -0600

    Update From Lakshan

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,10 @@ export const Footer = () => {
 
           <div>
             <h4 className="font-bold">Contact</h4>
-            <p className="text-sm text-muted-foreground">(630) 998-6671<br/>lostnfound@d125.org</p>
+            <p className="text-sm text-muted-foreground">
+              <a href="tel:(630) 998-6671" className="hover:underline">(630) 998-6671</a><br/>
+              <a href="mailto:lostnfound@d125.org" className="hover:underline">lostnfound@d125.org</a>
+            </p>
 
             <h4 className="font-bold mt-4">Web Team</h4>
             <p className="text-sm text-muted-foreground">Lakshan Suresh<br/>Ankith Nayak</p>
@@ -28,10 +31,10 @@ export const Footer = () => {
 
             <h4 className="font-bold mt-4">Website Sources</h4>
             <ul className="text-sm text-muted-foreground space-y-1">
-              <li>VS Code Licensing</li>
-              <li>React Licensing</li>
-              <li>Supabase Licensing</li>
-              <li>NPM Licensing</li>
+              <li><a href="https://code.visualstudio.com/license" className="hover:underline">VS Code Licensing</a></li>
+              <li><a href="https://github.com/facebook/react/blob/main/LICENSE" className="hover:underline">React Licensing</a></li>
+              <li><a href="https://github.com/supabase/supabase/blob/master/LICENSE" className="hover:underline">Supabase Licensing</a></li>
+              <li><a href="https://docs.npmjs.com/policies/npm-license" className="hover:underline">NPM Licensing</a></li>
             </ul>
           </div>
         </div>
